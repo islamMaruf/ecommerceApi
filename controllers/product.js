d930f2b7356e439e241e82547a7cb7f880e29025
islamMaruf <marufislam7424@gmail.com>
2021-03-26 12:40:14 +0600

    crud completed for product

--- controllers/product.js
+++ controllers/product.js
@@ -55,9 +55,61 @@ const show = (req, res) => {
   });
 };
 
-const update = {};
+const update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.log({ err });
+      return res.status(400).json({
+        error: "Image could not be uploaded",
+      });
+    }
+    const { name, description, price, category, quantity, shipping } = fields;
+    if (!name || !description || !price || !category || !quantity) {
+      return res.status(400).json({
+        err: "All fields are required",
+      });
+    }
+    let product = req.product;
+    product = _.extend(product, fields);
 
-const remove = {};
+    if (files.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1 mb",
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+    product.save((err, data) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      return res.status(201).json({
+        message: "Product updated successfully",
+        product: data,
+      });
+    });
+  });
+};
+
+const remove = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    return res.status(200).json({
+      message: "Product deleted successfully",
+    });
+  });
+};
 
 module.exports = {
   list,
--- routes/product.js
+++ routes/product.js
@@ -14,6 +14,14 @@ const { requireJWT } = require("../middleware/auth");
 
 router.post("/create/product/:userId", requireJWT, isAuth, isAdmin, create);
 router.get("/product/:productId", show);
+router.delete(
+  "/product/:productId/:userId",
+  requireJWT,
+  isAuth,
+  isAdmin,
+  remove
+);
+router.put("/product/:productId/:userId", requireJWT, isAuth, isAdmin, update);
 
 router.param("userId", userById);
 router.param("productId", productById);
